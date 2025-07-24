18cd6fb8e9eee4451437c2f2f55e4a6960a03ff2
code-with-divyanshu <divyanshubartwal67@gmail.com>
2025-07-25 02:14:23 +0530

    Updates Routes

--- src/routes/MainRoute.jsx
+++ src/routes/MainRoute.jsx
@@ -2,18 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Drinks from "../pages/Drinks";
-import DrinksDetails from "../pages/DrinksDetails";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import GetInTouch from "../pages/GetInTouch";
 
 const MainRoute = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/drinks" element={<Drinks />}>
-        <Route path="/drinks/:id" element={<DrinksDetails />} />
-      </Route>
+      <Route path="/drinks" element={<Drinks />} />
+      <Route path="/getintouch" element={<GetInTouch />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
     </Routes>
